5f44e5ad3556021089ecb65ce0478ec757de242c
Sona Muthuvijayan <smuthuvijayan@smvoaltembp.local>
2016-06-18 02:38:33 -0400

    Committing after a git add all to reflected renamed files

--- RxJavaSample/app/src/main/java/com/example/smuthuvijayan/rxsample/adapter/AppNetMessageAdapter.java
+++ RxJavaSample/app/src/main/java/com/example/smuthuvijayan/rxsample/adapter/AppNetMessageAdapter.java
@@ -1,7 +1,6 @@
 package com.example.smuthuvijayan.rxsample.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +9,6 @@ import android.widget.TextView;
 
 import com.example.smuthuvijayan.rxsample.R;
 import com.example.smuthuvijayan.rxsample.model.Datum;
-import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
