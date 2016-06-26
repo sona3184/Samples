2ad29231d17fb9596c712f47746b8c3f8b09056b
Sona Muthuvijayan <smuthuvijayan@smvoaltembp.local>
2016-06-26 02:16:20 -0400

    Cleanup - remove unused code, comments

--- RxJavaSample/app/src/main/java/com/karson/portfolio/adnfeed/service/AppNetService.java
+++ RxJavaSample/app/src/main/java/com/karson/portfolio/adnfeed/service/AppNetService.java
@@ -7,7 +7,6 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.google.common.collect.EvictingQueue;
 import com.karson.portfolio.adnfeed.Util;
 import com.karson.portfolio.adnfeed.model.AppNetData;
 import com.karson.portfolio.adnfeed.model.AppNetRowData;
@@ -21,7 +20,6 @@ import org.joda.time.DateTimeZone;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -30,9 +28,6 @@ import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
-import rx.subjects.PublishSubject;
-import rx.subjects.SerializedSubject;
-import rx.subjects.Subject;
 
 /**
  * Created by smuthuvijayan on 6/24/16.
@@ -46,8 +41,6 @@ public class AppNetService extends Service {
     private static final int APP_NET_REQUEST_DELAY_SEC = 3;
 
     private final IBinder mBinder = new AppNetServiceBinder();
-
-    /*Queue<AppNetRowData> appNewRowDataQueue = EvictingQueue.create(MAX_APP_NET_POSTS);*/
 
     final AppNetRestService appNetService = RestServiceFactory.createRetrofitService(AppNetRestService.class,
                                                             AppNetRestService.APP_NET_SERVICE_ENDPOINT);
@@ -64,68 +57,9 @@ public class AppNetService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(TAG, "onBind called");
-        //startSendingDummmyData();
         fetchAppNetData();
         return mBinder;
     }
-
-    private void startSendingDummmyData() {
-        Integer id = 1;
-
-        while (id <= 10) {
-            AppNetRowData data = new AppNetRowData(Integer.toString(id),
-                    "user" + id,
-                    "Post text " + id,
-                    "https://lh4.ggpht.com/mJDgTDUOtIyHcrb69WM0cpaxFwCNW6f0VQ2ExA7dMKpMDrZ0A6ta64OCX3H-NMdRd20=w300",
-                    new DateTime(DateTimeZone.UTC).toString());
-            AppNetRowDataBus.instanceOf().addAppNetRowData(data);
-            id++;
-        }
-    }
-
-   /* protected void fetchAppNetData() {
-        ScheduledExecutorService scheduler =
-                Executors.newSingleThreadScheduledExecutor();
-
-        scheduler.scheduleAtFixedRate
-                (new Runnable() {
-                    public void run() {
-                        appNetService.getGlobalMessages()
-                                .subscribeOn(Schedulers.newThread())
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .subscribe(new Subscriber<AppNetData>() {
-                                    @Override
-                                    public void onCompleted() {
-                                        Log.d(TAG, "Data get completed");
-                                    }
-
-                                    @Override
-                                    public void onError(Throwable e) {
-
-                                    }
-
-                                    @Override
-                                    public void onNext(AppNetData appNetData) {
-                                        Log.i(TAG, "Received data count = " + appNetData.getData().size());
-                                        for(Datum datum : appNetData.getData()) {
-                                            Log.i(TAG, datum.getId()
-                                                    + " | " + datum.getCreatedAt()
-                                                    + " | " + datum.getText()
-                                                    + " | " + datum.getUser().getUsername()
-                                                    + " | " + datum.getUser().getAvatarImage().getUrl());
-                                            AppNetRowData rowData = new AppNetRowData(datum.getId(),
-                                                    datum.getUser().getUsername(),
-                                                    datum.getText(),
-                                                    datum.getUser().getAvatarImage().getUrl(),
-                                                    datum.getCreatedAt());
-                                            AppNetRowDataBus.instanceOf().addAppNetRowData(rowData);
-                                        }
-                                    }
-                                });
-                    }
-                }, 0, 2, TimeUnit.SECONDS);
-
-    }*/
 
     protected void fetchAppNetData() {
         ScheduledExecutorService scheduler =
@@ -164,10 +98,10 @@ public class AppNetService extends Service {
     protected Observable<List<AppNetRowData>> fetchAppNetRowData() {
         Observable<AppNetData> data;
         if(Util.isNullOrEmpty(lastPostID)) {
-            Log.i(TAG, "Request last 20");
+            Log.d(TAG, "Request last 20");
             data = appNetService.getGlobalMessages();
         } else {
-            Log.i(TAG, "Request next 10 since " + lastPostID);
+            Log.d(TAG, "Request next 10 since " + lastPostID);
             data = appNetService.getGlobalMessages(lastPostID, APP_NET_REQUEST_BATCH_SIZE);
         }
         return data
